7959c067388071191afd7d07d71021bd602b3c5f
Steffan Carter <xisled09@gmail.com>
2022-05-21 16:26:50 -0400

    server js changes

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const path = require('path');
+const fs = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
